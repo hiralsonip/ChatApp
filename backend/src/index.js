0dc3a5b91b3d3882e37ff14a9c10334707185bda
hiralsonip <66827672+hiralsonip@users.noreply.github.com>
2025-05-23 19:30:01 -0400

    First Commit

---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+import express from "express"
+import dotenv from "dotenv"
+import authRoutes from "./routes/auth.route.js"
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser"
+
+const app = express();
+dotenv.config();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log("Server is running on port", PORT);
+    connectDB()
+})
