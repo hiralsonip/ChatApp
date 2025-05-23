5234bbb74cc8099c281cdd1519434f75a9b6c89b
hiralsonip <66827672+hiralsonip@users.noreply.github.com>
2025-05-23 19:52:15 -0400

    Message routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser"
 
@@ -10,6 +11,7 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("api/message", messageRoutes)
 
 const PORT = process.env.PORT
 
