92dd1f85111fd81714d6f71a4738a7015e3f54db
hiralsonip <66827672+hiralsonip@users.noreply.github.com>
2025-05-26 20:18:56 -0400

    Updated live link into code

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstace } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "https://chatapp-z726.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
     authUser: null,
