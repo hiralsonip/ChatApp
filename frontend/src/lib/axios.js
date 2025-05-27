460faa5438cf499fe0915fbaff0371280f5d7f28
hiralsonip <66827672+hiralsonip@users.noreply.github.com>
2025-05-26 23:04:02 -0400

    Resove path error

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstace = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chatapp-z726.onrender.com",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chatapp-z726.onrender.com/api",
     withCredentials: true
 })
